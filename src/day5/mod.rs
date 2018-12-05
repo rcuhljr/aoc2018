89ed0e48d3b785dea0ad12c87b666dece7e83b48
Robert C. Uhl <rcuhl@sep.com>
2018-12-05 11:49:29 -0500

    Holy speed up batman.

--- src/day5/mod.rs
+++ src/day5/mod.rs
@@ -13,41 +13,35 @@ pub fn solve_b() -> String {
 
 pub fn collapse_polymers(filename: String) -> String {
     collapse_polymers_string(
-        utility::load_strings(filename)
+        &utility::load_strings(filename)
             .iter()
             .next()
             .unwrap()
             .to_string(),
     )
 }
-pub fn collapse_polymers_string(polystring: String) -> String {
-    let mut polymers: Vec<char> = polystring.chars().collect();
 
-    let mut delete_at: i32 = -1;
-    let mut index: i32 = 0;
-    loop {
-        let mut last_char = ' ';
-        if delete_at >= 0 {
-            polymers.remove(delete_at as usize);
-            polymers.remove(delete_at as usize);
-            delete_at = -1
-        }
-        for char_index in index..(polymers.len() as i32) {
-            let item = polymers.get(char_index as usize).unwrap();
-            if last_char.to_ascii_uppercase() == item.to_ascii_uppercase() && last_char != *item {
-                delete_at = index - 1;
-                index = cmp::max(0, index - 2);
-                break;
+pub fn collapse_polymers_string(polystring: &String) -> String {
+    polystring
+        .chars()
+        .fold(Vec::new(), |mut remaining, newchar| {
+            if remaining.len() == 0 {
+                remaining.push(newchar);
+            } else {
+                let last_char = remaining[remaining.len() - 1];
+                if last_char.to_ascii_uppercase() == newchar.to_ascii_uppercase()
+                    && last_char != newchar
+                {
+                    remaining.pop();
+                } else {
+                    remaining.push(newchar);
+                }
             }
-            last_char = *item;
-            index += 1;
-        }
-        if delete_at == -1 {
-            break;
-        }
-    }
-
-    polymers.iter().map(|c| *c).collect()
+            remaining
+        })
+        .iter()
+        .map(|c| *c)
+        .collect()
 }
 
 static ASCII_LOWER: [char; 26] = [
@@ -69,7 +63,7 @@ fn find_shortest(filename: String) -> usize {
             .chars()
             .filter(|&c| !(c == *item || c == item2))
             .collect();
-        let result = collapse_polymers_string(mod_string);
+        let result = collapse_polymers_string(&mod_string);
         results.push(result.len());
     }
 
@@ -94,13 +88,13 @@ mod tests {
         assert_eq!(actual, 4);
     }
     // Uncomment when speed fix is in place.
-    // #[bench]
-    // fn bench_a(b: &mut Bencher) {
-    //     b.iter(|| solve_a());
-    // }
+    #[bench]
+    fn bench_a(b: &mut Bencher) {
+        b.iter(|| solve_a());
+    }
 
-    // #[bench]
-    // fn bench_b(b: &mut Bencher) {
-    //     b.iter(|| solve_b());
-    // }
+    #[bench]
+    fn bench_b(b: &mut Bencher) {
+        b.iter(|| solve_b());
+    }
 }
