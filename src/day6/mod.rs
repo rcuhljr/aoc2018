5aed1c48dcaa6951ca1513923f39782813b56862
Robert C. Uhl <rcuhl@sep.com>
2018-12-06 12:23:11 -0500

    Actually happy.

--- src/day6/mod.rs
+++ src/day6/mod.rs
@@ -9,71 +9,51 @@ pub fn solve_b() -> String {
     find_safe_area("input6.txt", 10000).to_string()
 }
 
-fn find_safe_area(filename: &str, cutoff: i32) -> i32 {
-    let mut max_dim = 0;
+fn parse_data(data: Vec<String>, goals: &mut HashMap<(i32, i32), i32>) -> i32 {
+    let mut max_dim: i32 = 0;
     let mut index: i32 = 0;
+    data.iter().for_each(|raw| {
+        let v: Vec<i32> = raw
+            .split(", ")
+            .map(|val| val.trim().parse().unwrap())
+            .collect();
+        goals.insert((v[0], v[1]), index);
+        if v[0] > max_dim {
+            max_dim = v[0]
+        }
+        if v[1] > max_dim {
+            max_dim = v[1]
+        }
+        index += 1;
+    });
+
+    max_dim + 1
+}
+
+fn find_safe_area(filename: &str, cutoff: i32) -> i32 {
     let mut goals: HashMap<(i32, i32), i32> = HashMap::new();
-    utility::load_strings(filename.to_string())
-        .iter()
-        .for_each(|raw| {
-            let v: Vec<i32> = raw
-                .split(", ")
-                .map(|val| val.trim().parse().unwrap())
-                .collect();
-            goals.insert((v[0], v[1]), index);
-            if v[0] > max_dim {
-                max_dim = v[0]
-            }
-            if v[1] > max_dim {
-                max_dim = v[1]
-            }
-            index += 1;
+    let size: i32 = parse_data(utility::load_strings(filename.to_string()), &mut goals);
+    let mut safe_areas = 0;
+
+    for index in 0..size * size {
+        let loc_x: i32 = (index as i32) % size;
+        let loc_y: i32 = (index as i32) / size;
+        let total_dist = goals.keys().fold(0, |acc, coords| {
+            acc + (coords.0 - loc_x).abs() + (coords.1 - loc_y).abs()
         });
-
-    let size: i32 = max_dim + 1;
-
-    let grid = vec![0; (size * size) as usize];
-
-    grid.iter()
-        .enumerate()
-        .map(|(index, _x)| {
-            let loc_x: i32 = (index as i32) % size;
-            let loc_y: i32 = (index as i32) / size;
-            if goals.keys().fold(0, |acc, coords| {
-                acc + (coords.0 - loc_x).abs() + (coords.1 - loc_y).abs()
-            }) < cutoff
-            {
-                1
-            } else {
-                0
-            }
-        }).fold(0, |acc, x| acc + x)
+        if total_dist < cutoff {
+            safe_areas += 1;
+        }
+    }
+    safe_areas
 }
 
 fn find_largest_area(filename: &str) -> i32 {
-    let mut max_dim = 0;
-    let mut index = 0;
-    let mut goals: HashMap<(i32, i32), i32> = HashMap::new();
     let mut infinite = HashMap::new();
-    utility::load_strings(filename.to_string())
-        .iter()
-        .for_each(|raw| {
-            let v: Vec<i32> = raw
-                .split(", ")
-                .map(|val| val.trim().parse().unwrap())
-                .collect();
-            goals.insert((v[0], v[1]), index);
-            if v[0] > max_dim {
-                max_dim = v[0]
-            }
-            if v[1] > max_dim {
-                max_dim = v[1]
-            }
-            index += 1;
-        });
-    let size: i32 = max_dim + 1;
+    let mut goals: HashMap<(i32, i32), i32> = HashMap::new();
+    let mut result = HashMap::new();
+    let size: i32 = parse_data(utility::load_strings(filename.to_string()), &mut goals);
 
-    let mut grid: Vec<i32> = Vec::new();
     for index in 0..(size * size) {
         let loc_x: i32 = (index as i32) % size;
         let loc_y: i32 = (index as i32) / size;
@@ -84,20 +64,11 @@ fn find_largest_area(filename: &str) -> i32 {
                 (dist, *id)
             }).collect();
         distances.sort_by_key(|pair| pair.0);
-        if distances[0].0 == distances[1].0 {
-            grid.push(-1);
-        } else {
+        if distances[0].0 != distances[1].0 {
             if loc_x == 0 || loc_x == size - 1 || loc_y == 0 || loc_y == size - 1 {
                 infinite.insert(distances[0].1.clone(), true);
             }
-            grid.push(distances[0].1);
-        }
-    }
-
-    let mut result = HashMap::new();
-    for entry in grid.iter() {
-        if *entry >= 0 {
-            let counter = result.entry(entry).or_insert(0);
+            let counter = result.entry(distances[0].1).or_insert(0);
             *counter += 1;
         }
     }
