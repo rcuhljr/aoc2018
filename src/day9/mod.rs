e26130f11f288f3cbe6c138a7fdd1a397bac864b
Robert Uhl <rcuhljr@gmail.com>
2018-12-09 02:11:48 -0500

    Minor tweaks.

--- src/day9/mod.rs
+++ src/day9/mod.rs
@@ -1,28 +1,30 @@
 use std::collections::LinkedList;
 
 pub fn solve_a() -> String {
-    play_game(419, 71052).1.to_string()
+    play_game(419, 71052).to_string()
 }
 
 pub fn solve_b() -> String {
-    play_game(419, 7105200).1.to_string()
+    play_game(419, 7105200).to_string()
 }
 
 fn move_cw<T>(list: &mut LinkedList<T>, offset: usize) {
+    let mut temp;
     for _ in 0..offset {
-        let temp = list.pop_front().unwrap();
+        temp = list.pop_front().unwrap();
         list.push_back(temp);
     }
 }
 
 fn move_ccw<T>(list: &mut LinkedList<T>, offset: usize) {
+    let mut temp;
     for _ in 0..offset {
-        let temp = list.pop_back().unwrap();
+        temp = list.pop_back().unwrap();
         list.push_front(temp);
     }
 }
 
-fn play_game(players: usize, last_marble: usize) -> (usize, usize) {
+fn play_game(players: usize, last_marble: usize) -> usize {
     let mut scores = vec![0; players];
     let mut circle: LinkedList<usize> = LinkedList::new();
     circle.push_front(0);
@@ -37,12 +39,7 @@ fn play_game(players: usize, last_marble: usize) -> (usize, usize) {
         }
     }
 
-    scores
-        .iter()
-        .enumerate()
-        .max_by_key(|score| *score.1)
-        .map(|x| (x.0, *x.1))
-        .unwrap()
+    *scores.iter().max().unwrap()
 }
 
 #[cfg(test)]
@@ -52,13 +49,13 @@ mod tests {
     #[test]
     fn should_solve_sample9a() {
         let game_score = play_game(9, 25);
-        assert_eq!(game_score, (5, 32));
+        assert_eq!(game_score, 32);
     }
 
     #[test]
     fn should_solve_another_sample9a() {
         let game_score = play_game(17, 1104);
-        assert_eq!(game_score, (16, 2764));
+        assert_eq!(game_score, 2764);
     }
 
 }
