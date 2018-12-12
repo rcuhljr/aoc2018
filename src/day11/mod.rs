6c64db504287ed252df977375b618906dddfd0aa
Robert Uhl <rcuhljr@gmail.com>
2018-12-11 21:26:11 -0500

    Swanson with the summed area table find.

--- src/day11/mod.rs
+++ src/day11/mod.rs
@@ -1,7 +1,7 @@
 use std::cmp;
 
 pub fn solve_a() -> String {
-    let res = locate_densest_area(5153);
+    let res = locate_densest_area(5153, 300);
     format!("{},{}", res.0, res.1)
 }
 
@@ -11,19 +11,17 @@ pub fn solve_b() -> String {
 }
 
 fn locate_densest_var_area(serial: i32, size: usize) -> (i32, i32, i32) {
-    let grid = build_power_grid(serial, size as i32);
+    let mut grid = build_power_grid(serial, size as i32);
+    build_summed_table(&mut grid);
     let mut max = (0, 0, 0, 0);
 
-    for x in 1..size + 1 {
-        for y in 1..size + 1 {
-            let mut sum = grid[x][y];
+    for x in 1..size {
+        for y in 1..size {
             let max_size = size + 1 - cmp::max(x, y);
             for i in 1..max_size {
-                for j in 1..i + 1 {
-                    sum += grid[x + i][y + j - 1];
-                    sum += grid[x + j - 1][y + i];
-                }
-                sum += grid[x + i][y + i];
+                let sum = grid[x + i][y + i] + grid[x - 1][y - 1]
+                    - grid[x + i][y - 1]
+                    - grid[x - 1][y + i];
                 if sum > max.2 {
                     max = (x, y, sum, i + 1);
                 }
@@ -34,18 +32,15 @@ fn locate_densest_var_area(serial: i32, size: usize) -> (i32, i32, i32) {
     (max.0 as i32, max.1 as i32, max.3 as i32)
 }
 
-fn locate_densest_area(serial: i32) -> (i32, i32) {
-    let grid = build_power_grid(serial, 300);
+fn locate_densest_area(serial: i32, size: usize) -> (i32, i32) {
+    let mut grid = build_power_grid(serial, size as i32);
+    build_summed_table(&mut grid);
     let mut max = (0, 0, 0);
 
-    for x in 1..298 {
-        for y in 1..298 {
-            let mut sum = 0;
-            for i in 0..3 {
-                for j in 0..3 {
-                    sum += grid[x + i][y + j];
-                }
-            }
+    for x in 1..size - 2 {
+        for y in 1..size - 2 {
+            let sum =
+                grid[x + 2][y + 2] + grid[x - 1][y - 1] - grid[x + 2][y - 1] - grid[x - 1][y + 2];
             if sum > max.2 {
                 max = (x, y, sum);
             }
@@ -61,7 +56,11 @@ fn build_power_grid(serial: i32, size: i32) -> Vec<Vec<i32>> {
     for x in 0..size + 1 {
         let mut col: Vec<i32> = Vec::new();
         for y in 0..size + 1 {
-            col.push(calc_power(serial, x, y));
+            if x == 0 || y == 0 {
+                col.push(0);
+            } else {
+                col.push(calc_power(serial, x, y));
+            }
         }
         grid.push(col);
     }
@@ -73,13 +72,21 @@ fn calc_power(serial: i32, x: i32, y: i32) -> i32 {
     (((rack_id * y + serial) * rack_id) % 1000) / 100 - 5
 }
 
+fn build_summed_table(grid: &mut Vec<Vec<i32>>) {
+    for x in 1..grid.len() {
+        for y in 1..grid.len() {
+            grid[x][y] += grid[x][y - 1] + grid[x - 1][y] - grid[x - 1][y - 1]
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn should_solve_sample11a_for_time() {
-        let actual = locate_densest_area(18);
+        let actual = locate_densest_area(18, 300);
         assert_eq!(actual, (33, 45));
     }
 
@@ -97,6 +104,25 @@ mod tests {
         assert_eq!(actual[217][196], 0);
         let actual = build_power_grid(71, 300);
         assert_eq!(actual[101][153], 4);
+    }
+
+    #[test]
+    fn should_make_sat() {
+        let mut actual = build_power_grid(18, 3);
+        assert_eq!(
+            actual,
+            [[0, 0, 0, 0], [0, -2, -1, 0], [0, -2, 0, 1], [0, -1, 0, 2]]
+        );
+        build_summed_table(&mut actual);
+        assert_eq!(
+            actual,
+            [
+                [0, 0, 0, 0],
+                [0, -2, -3, -3],
+                [0, -4, -5, -4],
+                [0, -5, -6, -3]
+            ]
+        );
     }
 
     #[test]
